e939afff3cab73405e78d9c9871a7f287fd833d4
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-07-24 10:31:47 +0300

    hero component added to index page

---
+++ components/Hero.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="mx-40 h-screen">
+      <Image
+        width={4693}
+        height={3126}
+        layout="intrinsic"
+        src="/images/hero.jpg"
+      />
+    </div>
+  );
+}
