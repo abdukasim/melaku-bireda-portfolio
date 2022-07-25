d63a7eb3b31e0eeb3e85533ab1442128c18aa20a
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-07-25 09:28:24 +0300

    added about section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import About from "../components/About";
 import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Hero />
+      <About />
     </>
   );
 }
