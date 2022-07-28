1b6ab9f768f19d8f2a5842298a3776bd9a29e476
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-07-28 13:39:57 +0300

    grid layout change and added auto animate(not working)

--- components/Music.js
+++ components/Music.js
@@ -1,13 +1,17 @@
 import React from "react";
-
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 export default function Music() {
+  const [animationParent] = useAutoAnimate();
   return (
-    <div id="music" className="mb-16 lg:mb-32">
+    <div id="music" className="mb-16  lg:px-60">
       <h1 className="heading-1 text-center mb-10">Music</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mx-20 lg:mx-40">
-        <div className="w-full aspect-video">
+      <div
+        className="grid grid-cols-1 lg:grid-cols-3 gap-4 mx-20 lg:mx-40"
+        ref={animationParent}
+      >
+        <div className="w-full aspect-video lg:col-span-3">
           <iframe
-            src="https://www.youtube.com/embed/ZzSUtMQOXdE"
+            src="https://www.youtube.com/embed/Flh8KeCDDOM"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -34,7 +38,7 @@ export default function Music() {
         </div>
         <div className="w-full aspect-video">
           <iframe
-            src="https://www.youtube.com/embed/Flh8KeCDDOM"
+            src="https://www.youtube.com/embed/ZzSUtMQOXdE"
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
