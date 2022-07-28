eae7f843ea082cec55684c6f80f804ec646f1507
Abdulrahman Kasim <abdukasimr@gmail.com>
2022-07-28 13:36:33 +0300

    added margin to heading

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
       id="contact"
       className="flex flex-col justify-center items-center mb-32"
     >
-      <h1 className="heading-1">Contact Us</h1>
+      <h1 className="heading-1 mb-10">Contact Us</h1>
       <div className="flex flex-col w-96">
         <label htmlFor="name" className="mb-2">
           Full Name
